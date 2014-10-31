c25b0067633e6847b12cbbb097bd4c14fe2bcf63
Prashanth.Kumar <micprashanth@gmail.com>
2014-10-31 15:30:08 +0530

    edit:Toast.java

--- src/Toast.java
+++ src/Toast.java
@@ -12,23 +12,18 @@ public class Toast extends CordovaPlugin {
     public boolean execute(String action, JSONArray args, final CallbackContext callbackContext) throws JSONException {
         if (action.equals("show")) {
             final String msg = args.getString(0);
-            final String pos = args.getString(1);
+            final String pos =  args.getString(1);
             cordova.getActivity().runOnUiThread(new Runnable(){
                 public void run(){
                     android.widget.Toast toast = android.widget.Toast.makeText(webView.getContext(),msg,android.widget.Toast.LENGTH_SHORT);
-                    switch(pos){
-                        case "top":
-                            toast.setGravity(android.view.Gravity.TOP,0,0);
-                            break;
-                        case "center":
-                            toast.setGravity(android.view.Gravity.CENTER,0,0);
-                            break;
-                        case "bottom":
-                            toast.setGravity(android.view.Gravity.BOTTOM,0,0);
-                            break;
-                        default:
-                            toast.setGravity(android.view.Gravity.CENTER,0,0);
-                            break;
+                    if(pos=="top"){
+                        toast.setGravity(android.view.Gravity.TOP,0,0);
+                    }else if(pos=="center"){
+                        toast.setGravity(android.view.Gravity.CENTER,0,0);
+                    }else if(pos=="bottom"){
+                        toast.setGravity(android.view.Gravity.BOTTOM,0,0);
+                    }else {
+                        toast.setGravity(android.view.Gravity.CENTER,0,0);
                     }
                     toast.show();
                     callbackContext.success();
