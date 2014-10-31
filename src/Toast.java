e5a15ce82de26f31617b550fa78073ddb4f141fc
Prashanth.Kumar <micprashanth@gmail.com>
2014-10-31 15:24:54 +0530

    edit:Toast.java

--- src/Toast.java
+++ src/Toast.java
@@ -12,9 +12,24 @@ public class Toast extends CordovaPlugin {
     public boolean execute(String action, JSONArray args, final CallbackContext callbackContext) throws JSONException {
         if (action.equals("show")) {
             final String msg = args.getString(0);
+            final String pos = args.getString(1);
             cordova.getActivity().runOnUiThread(new Runnable(){
                 public void run(){
                     android.widget.Toast toast = android.widget.Toast.makeText(webView.getContext(),msg,android.widget.Toast.LENGTH_SHORT);
+                    switch(pos){
+                        case "top":
+                            toast.setGravity(android.view.Gravity.TOP);
+                            break;
+                        case "center":
+                            toast.setGravity(android.view.Gravity.CENTER);
+                            break;
+                        case "bottom":
+                            toast.setGravity(android.view.Gravity.BOTTOM);
+                            break;
+                        default:
+                            toast.setGravity(android.view.Gravity.CENTER);
+                            break;
+                    }
                     toast.show();
                     callbackContext.success();
                     return ;
--- www/toast.js
+++ www/toast.js
@@ -1,8 +1,26 @@
 var exec = require('cordova/exec');
 var Toast = {};
 
-Toast.show=function(str,callback){
-	exec(function(){},function(){},"Toast","show",[str]);
+Toast.show=function(str,place,callback){
+
+	switch(place){
+
+		case "top":
+			exec(function(){},function(){},"Toast","show",[str,"top"]);
+			break;	
+		case "center":
+			exec(function(){},function(){},"Toast","show",[str,"center"]);	
+			break;
+		case "bottom"
+			exec(function(){},function(){},"Toast","show",[str,"bottom"]);	
+			break;
+		default:
+			exec(function(){},function(){},"Toast","show",[str,"center"]);	
+			break;
+
+	}
+
+	
 }
 
 module.exports=Toast;
